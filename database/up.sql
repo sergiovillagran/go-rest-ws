667afda3012f31183f7d0e32e5c9cb2fb32c21b3
Sergio Villagran <sergio.villagran@3dves.com>
2022-12-05 22:28:57 -0500

    Add database conection url and correct the sql up script

--- database/up.sql
+++ database/up.sql
@@ -1,8 +1,8 @@
-DROP TABLE IF EXISTS users
+DROP TABLE IF EXISTS users;
 
-CREATE TABLE users {
+CREATE TABLE users (
     id VARCHAR(32) PRIMARY KEY,
     password VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW()
-}
+);
