5f4b1dbebfacc08506cbf28c19fe4d6f2ec8b4bb
Sergio Villagran <sergio.villagran@3dves.com>
2022-12-14 07:09:52 -0500

    We some general modifications

--- middleware/auth.go
+++ middleware/auth.go
@@ -10,13 +10,13 @@ import (
 )
 
 var (
-	NO_AUTH_NEEDED = string[] {
+	NO_AUTH_NEEDED = []string{
 		"login",
-		"signup"
+		"signup",
 	}
 )
 
-func shouldCheckToken (route string) bool {
+func shouldCheckToken(route string) bool {
 	for _, p := range NO_AUTH_NEEDED {
 		if strings.Contains(route, p) {
 			return false
@@ -28,13 +28,13 @@ func shouldCheckToken (route string) bool {
 func CheckAuth(s server.Server) func(h http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			if(!shouldCheckToken(r.URL.Path)) {
+			if !shouldCheckToken(r.URL.Path) {
 				next.ServeHTTP(w, r)
 				return
 			}
 
 			tokenString := strings.TrimSpace(r.Header.Get("Authorization"))
-			_, err := jwt.ParseWithClaims(tokenString, models.AppClaims{}, func(token *jwt.Token) (interface {}, error) {
+			_, err := jwt.ParseWithClaims(tokenString, models.AppClaims{}, func(token *jwt.Token) (interface{}, error) {
 				return []byte(s.Config().JWTSecret), nil
 			})
 
